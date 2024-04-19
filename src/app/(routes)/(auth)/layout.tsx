ba551f406ebaefa66c63d164c1a313099dabcab6
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-04-19 00:00:08 -0400

    Basic auth setup with Clerk

---
+++ src/app/(routes)/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">{children}</div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import Footer from "./_components/footer";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import Navbar from "./_components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html className="h-full" lang="en">
+        <body className={`h-full ${inter.className}`}>
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
