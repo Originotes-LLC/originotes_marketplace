315ad2e73809643287692d10b3edf3053d2b8cdf
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-05-07 23:46:08 -0400

    bug: fixed all typescript errors

--- src/app/_lib/schema.ts
+++ src/app/_lib/schema.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
-
+// const ACCEPTED_IMAGE_TYPES = [
+//   "image/jpeg",
+//   "image/jpg",
+//   "image/png",
+//   "image/webp",
+// ];
 export const ServiceListingSchema = z.object({
   service_name: z
     .string()
@@ -10,9 +15,16 @@ export const ServiceListingSchema = z.object({
     .min(10, "Service description must be at least 10 characters")
     .max(500, "Service description must be at most 500 characters"),
   service_category: z.string(),
-  // service_file_upload: z.instanceof(File),
   service_price: z.string({
     required_error:
       "Service price is required. Please enter a valid price for your service",
   }),
+  service_files: z.any().refine((file: string) => {
+    return true;
+  }, "Please update or add new image."),
+
+  // .refine((file) => {
+  //   console.log("does these files get accepted in the backend?: ", file);
+  //   return ACCEPTED_IMAGE_TYPES.includes(file?.type);
+  // }, ".jpg, .jpeg, .png and .webp files are accepted."),
 });
