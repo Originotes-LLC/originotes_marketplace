750498580f7c3db0f03afe34e5f30ff02f073e71
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-06-03 22:52:27 -0400

    chore: modified the test-1 example

--- src/tests/sign-in.spec.ts
+++ src/tests/sign-in.spec.ts
@@ -3,9 +3,5 @@ import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await setupClerkTestingToken({ page });
-  await page.goto("http://localhost:3000/");
-  await page.getByRole("button", { name: "Sign in" }).click();
-  await page
-    .getByRole("button", { name: "Sign in with Google Google" })
-    .click();
+  await page.goto("/");
 });
